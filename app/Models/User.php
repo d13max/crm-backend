564e9bef97df7053bcd49e4e50555f916843f159
Maxim <d13.max@gmail.com>
2026-03-14 21:09:58 +0300

    feat: api tokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,18 +1,22 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasUuids;
+
+    /** @use HasApiTokens<PersonalAccessToken> */
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
